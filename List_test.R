c06f98f12ce4cfc256369e017ad3dfb4e603f07b
eherdter <eherdter@mail.usf.edu>
2015-02-09 11:56:36 -0500

    deleted increment number index

--- List_test.R
+++ List_test.R
@@ -1,6 +1,6 @@
 mydata= read.csv('BC_6_23_14.csv',header=TRUE)
 
-newdata <- subset(mydata, Increment.Number ==3 | Increment.Number==4 | Increment.Number==5 | Increment.Number==6 | Increment.Number==7, select=c(Year.of.Increment.Formation, Increment.Width, Increment.Number, Catch.Year, Age.at.Catch))
+newdata <- subset(mydata, Increment.Number ==3 | Increment.Number==4 | Increment.Number==5, select=c(Year.of.Increment.Formation, Increment.Width, Increment.Number, Catch.Year, Age.at.Catch))
 
 
 newdata$Increment.Number = factor(newdata$Increment.Number)
